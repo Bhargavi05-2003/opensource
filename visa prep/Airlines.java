8ef6fb27a1832726efc1af1b52c843bfea99d86a
Bhargavi05-2003 <bhargaviyellavula@gmail.com>
2024-11-18 20:08:02 +0530

    Create Airlines.java

---
+++ visa prep/Airlines.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new
+            Scanner(System.in);
+        int x=sc.nextInt();
+        int n=sc.nextInt();
+        int a=(n+99)/100;
+        int b=a-x;
+        System.out.println(b);
+        
+        
+    }
+}
